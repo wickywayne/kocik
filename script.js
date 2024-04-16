8de821f8c41b7df699b6aef54e077d5786df58a9
wickywayne <131608086+wickywayne@users.noreply.github.com>
2024-04-16 18:01:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelector(".przycisk").addEventListener("click", losujFotke);
+});
+
+function losujFotke() {
+    const zdjeciaKotow = [
+        { src: "kot1.jpg", podpis: "Kocik glupiutki" },
+        { src: "kot2.jpg", podpis: "Kocik powazniutki" },
+        { src: "kot3.jpg", podpis: "Kocik uszaty" },
+        { src: "kot4.jpg", podpis: "Kocik kochany" },
+        { src: "kot5.jpg", podpis: "Kocik jablkowy" },
+        { src: "kot6.jpg", podpis: "Kocik zdezorientowany" },
+        { src: "kot7.jpg", podpis: "Kocik benc" },
+        { src: "kot8.jpg", podpis: "Kocik arbuzowy" },
+        { src: "kot9.jpg", podpis: "Kocik nerd" },
+        { src: "kot10.jpg", podpis: "Kocik malutki" }
+    ];
+    
+    const losowyIndeks = Math.floor(Math.random() * zdjeciaKotow.length);  
+    const imgElement = document.getElementById("imgKocik");
+    const podpisElement = document.querySelector(".podpis");
+
+    imgElement.src = zdjeciaKotow[losowyIndeks].src;
+    podpisElement.textContent = zdjeciaKotow[losowyIndeks].podpis;
+}
